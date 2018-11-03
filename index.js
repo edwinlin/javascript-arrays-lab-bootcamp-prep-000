505a50ce6904d00fa4a10db1c0eb339b09554372
Edwin Lin <edwinlin@gmail.com>
2018-11-03 06:31:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(){
+function destructivelyAppendKitten(name){
   
 }
+
+function destructivelyPrependKitten(name){
+  
+}
+
+function 
